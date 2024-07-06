3fbe2fa5bc2c64c8fe5e633274182009d2f52e93
limfakson <fakeyejoshua2005@gmail.com>
2024-07-06 22:31:42 +0100

    chapter 7

--- base.py
+++ base.py
@@ -1,11 +1,23 @@
 import kivy
 from kivy.app import App
-from kivy.uix.widget import Widget       
-        
-class MyApp(App):
+from kivy.uix.screenmanager import Screen, ScreenManager    
+from kivy.lang import Builder
+
+class MainScreen(Screen):
+    pass
+
+class SecondScreen(Screen):
+    pass
+
+class WindowManager(ScreenManager):
+    pass
+
+kv = Builder.load_file('my.kv')
+
+class MyMainApp(App):
     def build(self):
-        return    
+        return kv
         
 
 if __name__ == '__main__':
-    MyApp().run()
+    MyMainApp().run()
